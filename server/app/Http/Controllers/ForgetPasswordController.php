d3a26ce06f3da6e299897efa7ee23fb95a8541a2
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-08 14:52:05 +0900

    Laravel Passport Authentication : Forget Password Part2

--- server/app/Http/Controllers/ForgetPasswordController.php
+++ server/app/Http/Controllers/ForgetPasswordController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ForgetPasswordRequest;
+use App\Mail\ForgetPasswordMail;
 use Illuminate\Http\Request;
-use app\Models\User;
+use App\Models\User;
 use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class ForgetPasswordController extends Controller
 {
@@ -13,7 +16,7 @@ class ForgetPasswordController extends Controller
     {
         $email = $request->email;
 
-        if (User::where('email', $email)->dosentExist()) {
+        if (User::where('email', $email)->doesntExist()) {
             return response([
                 'message' => 'Email Invalid',
             ], 401);
@@ -23,7 +26,17 @@ class ForgetPasswordController extends Controller
         $token = rand(10, 100000);
 
         try {
-            
+            DB::table('password_resets')->insert([
+                'email' => $email,
+                'token' => $token,
+            ]);
+
+            // Mail Send to User
+            Mail::to($email)->send(new ForgetPasswordMail($token));
+
+            return response([
+                'message' => 'Reset Password Mail send on your email',
+            ], 200);
         } catch (Exception $exception) {
             return response([
                 'message' => $exception->getMessage()
